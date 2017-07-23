5415c7d642577a914c87557749c6c789673f93b7
Kevin Coyner <rustybear@gmail.com>
2017-07-23 10:36:55 -0400

    doodle: 6 tests passing. Added handleApiRequest.

--- server/request-handler.js
+++ server/request-handler.js
@@ -28,64 +28,56 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
+// const { URL } = require('url').URL;
+const url = require('url');
 
 module.exports = {
 
+  requestHandler: function(request, response) {
+    // console.log('Serving request type ' + request.method + ' for url ' + request.url);
+    // console.log('request.headers: ', request.headers);
+    var urlParsed = url.parse(request.url);
 
-  requestHandler: function (request, response) {
-  // var requestHandler = function(request, response) {
-    // Request and Response come from node's http module.
-    //
-    // They include information about both the incoming request, such as
-    // headers and URL, and about the outgoing response, such as its status
-    // and content.
-    //
-    // Documentation for both request and response can be found in the HTTP section at
-    // http://nodejs.org/documentation/api/
+    if (request.method === 'GET') {
+      handleGetRequest(response, urlParsed);
+    } else if (['POST', 'PUT', 'DELETE'].indexOf(request.method) > -1) {
+      handleApiRequest(response, urlParsed, request.method);
+    } else {
+      response.end('Method not supported');
+    }
+  }
+};
 
-    // Do some basic logging.
-    //
-    // Adding more logging to your server can be an easy way to get passive
-    // debugging help, but you should always be careful about leaving stray
-    // console.logs in your code.
-    console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-    // The outgoing status.
-    // var statusCode = 200;
-    statusCode = 200;
-
-    // See the note above about CORS headers.
-    headers = defaultCorsHeaders;
-
-    // Tell the client we are sending them plain text.
-    //
-    // You will need to change this if you are sending something
-    // other than plain text, like JSON or HTML.
-    // headers['Content-Type'] = 'text/plain';
-    headers['Content-Type'] = 'application/json';
+handleGetRequest = function(res, urlParsed) {
+  console.log('urlParsed.path: ', urlParsed.path);
+  if (urlParsed.path === '/classes/messages' || urlParsed.path === '/.favicon.ico' || urlParsed.path === '/') {
 
     var testObj = {
       results: []
     };
+    statusCode = 200;
+    headers = defaultCorsHeaders;
+    headers['Content-Type'] = 'application/json';
+    res.writeHead(statusCode, headers);
+    res.write(JSON.stringify(testObj) + '\n');
+    res.end();
+
+  } else {
+    res.statusCode = 404;
+    res.end('404\n');
 
 
-    // .writeHead() writes to the request line and headers of the response,
-    // which includes the status and all headers.
-    response.writeHead(statusCode, headers);
-
-    response.write(JSON.stringify(testObj));
-    // Make sure to always call response.end() - Node may not send
-    // anything back to the client until you do. The string you pass to
-    // response.end() will be the body of the response - i.e. what shows
-    // up in the browser.
-    //
-    // Calling .end "flushes" the response's internal buffer, forcing
-    // node to actually send all the data over to the client.
-    response.end();
   }
-
-// exports.requestHandler = requestHandler;
-
 };
 
+handleApiRequest = function(res, urlParsed, method) {
+  if (urlParsed.path !== '/classes/messages') {
+    res.statusCode = 404;
+    res.end('404\n');
+  }
+  console.log('API ONLY');
+  console.log('urlParsed.path: ', urlParsed.path);
+  res.end();
+};
 
