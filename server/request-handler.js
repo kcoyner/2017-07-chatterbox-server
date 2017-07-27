1f6413a85bcdeec37a140a3c14b5ffb09821fa39
Kevin Coyner <rustybear@gmail.com>
2017-07-27 16:29:00 -0400

    doodle: Passing all 'server' tests but not the listening server tests.

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,19 +1,11 @@
-/*************************************************************
+/**
+ * server/request-handler.js
+ */
 
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-var fs = require('fs');
+const fs = require('fs');
 const path = require('path');
-var url = require('url');
+const url = require('url');
+const FILENAME = path.join(process.cwd(), 'server/classes/messages');
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -39,7 +31,8 @@ var filterData = function(file, queryString) {
     if (key === 'username' || key === 'roomname') {
       file = file.filter(objs => objs[key] === queryString[key]);
     } else if (key === 'order') {
-      // sort by date
+      // TODO: sort by date by examining createdAt
+      file = file;
     } else if (key === 'limit') {
       file = file.slice(0, queryString[key]);
     }
@@ -47,77 +40,69 @@ var filterData = function(file, queryString) {
   return file;
 };
 
-var handleGetRequest = function(request, response, pathName, queryString) {
-  // console.log('pathName: ', pathName);
-  var filename = path.join(process.cwd(), 'server' + pathName);
-  // console.log('PWD: ', process.env.PWD)
-  fs.exists(filename, function(exists) {
-    if (!exists) {
-      response.writeHead(404, {
-        'Content-Type': 'text/plain'
-      });
-      // response.write('404 Not Found\n');
-      response.end('404 Not Found\n');
-      return;
-    }
+var return404 = function(response) {
+  response.writeHead(404, {
+    'Content-Type': 'text/plain'
+  });
+  response.end('404 Not Found\n');
+};
 
-    fs.readFile(filename, 'binary', function(err, file) {
+var return500 = function(response, err) {
+  console.error('FILENAME not found: ', err + '\n');
+  response.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+  response.end('Error: File not found. StatusCode 500');
+};
+
+var handleGetRequest = function(request, response) {
+  if (url.parse(request.url).pathname !== '/classes/messages') {
+    return404(response);
+  } else {
+    var queryString = url.parse(request.url, true).query;
+
+    fs.readFile(FILENAME, 'binary', function(err, file) {
       if (err) {
-        response.writeHead(500, {
-          'Content-Type': 'text/plain'
-        });
-        // response.write(err + '\n');
-        response.end(err + '\n');
+        return500(response, err);
         return;
       }
       var resultsObj = {};
       resultsObj.results = JSON.parse(file);
       var finalResult = {};
       finalResult.results = filterData(resultsObj.results, queryString);
-      // statusCode = 200;
-      headers = defaultCorsHeaders;
+      var headers = defaultCorsHeaders;
       headers['Content-Type'] = 'application/json';
       response.writeHead(200, headers);
       response.end(JSON.stringify(finalResult));
     });
-  });
+  }
 };
 
-var requestHandler = function(request, response) {
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
+var handleOptionsRequest = function(request, response) {
   var headers = defaultCorsHeaders;
-
   headers['Content-Type'] = 'text/plain';
+  response.writeHead(200, headers);
+  response.end();
+};
 
-  var urlParsed = url.parse(request.url, true);
-  var pathName = urlParsed.pathname;
-  var queryString = urlParsed.query;
-  // build path name of file messages
-  // var filename = path.join(process.cwd(), pathName);
-  var filename = path.join(process.cwd(), 'server' + pathName);
-  // if (pathName === '/classes/messages') {
-
-  if (request.method === 'OPTIONS') {
-    response.writeHead(200, headers);
-    response.end();
-  } else if (request.method === 'GET') {
-    handleGetRequest(request, response, pathName, queryString);
-
-  } else if (request.method === 'POST') {
+var handlePostRequest = function(request, response) {
+  if (url.parse(request.url).pathname !== '/classes/messages') {
+    return404(response);
+  } else {
     var message = '';
     request.on('data', function(chunk) {
       message += chunk;
-      message = JSON.parse(message);
-      response.writeHead(201, headers);
-
-      fs.readFile(filename, function(err, messagesIn) {
+      console.log('message: ', message);
+    }).on('end', function() {
+      message = JSON.parse(message || 'null');  // TODO: this is a problem
+      if (!message) {
+        console.log('help');
+      }
+      fs.readFile(FILENAME, function(err, messagesIn) {
         if (err) {
-          console.error(err);
+          return500(response, err);
+          return;
         }
-
         messagesIn = JSON.parse(messagesIn);
         // add createdAt and updatedAt keys
         var jsonDate = (new Date()).toJSON();
@@ -135,7 +120,7 @@ var requestHandler = function(request, response) {
         message.objectId = createObjectId();
         messagesIn.push(message);
 
-        fs.writeFile(filename, JSON.stringify(messagesIn), function(err) {
+        fs.writeFile(FILENAME, JSON.stringify(messagesIn), function(err) {
           if (err) {
             console.error(err);
           }
@@ -143,34 +128,34 @@ var requestHandler = function(request, response) {
         });
       });
 
-    });
-    request.on('end', function() {
-      response.writeHead(201, headers);
-      // response.end('Successfully posted!');
-      response.end();
-    });
+    // }).on('end', function() {
 
-  } else if (request.url) {
+      var headers = defaultCorsHeaders;
+      headers['Content-Type'] = 'application/json';
+      response.writeHead(201, headers);
+      response.end(() => console.log('POST complete'));
+    });
+  }
+};
+
+var requestHandler = function(request, response) {
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  const { headers, method, url } = request;
+
+  if (method === 'OPTIONS') {
+    handleOptionsRequest(request, response);
+  } else if (method === 'GET') {
+    handleGetRequest(request, response);
+  } else if (method === 'POST') {
+    handlePostRequest(request, response);
+  } else {
     if (request.url === '/') {
       request.url = '/client/index.html';
     }
   }
-  // } //else {
-  // response.writeHead(404, headers);
-  // response.end('could not query request');
-  // }
 };
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 
 module.exports = {
   requestHandler: requestHandler
 };
+
